aac3798ac9c28f725cde4fa2a6fb4ea47755db20
evergreen96 <jsr27382@gmail.com>
2020-08-04 12:09:08 +0900

    binary search tree

--- 문제풀이 전략/binary_search_tree.cpp
+++ 문제풀이 전략/binary_search_tree.cpp
@@ -9,6 +9,12 @@ struct Node
 
 Node *head, *end;
 
+void initialize()
+{
+    head->key = end->key = -1;
+    head->left = head->right = end->left = end->right = end;
+}
+
 void insert(int num)
 {
     Node *parent, *child;
@@ -29,6 +35,62 @@ void insert(int num)
         parent->left = child;
     else
         parent->right = child;
+}
+
+void deletetion(int num)
+{
+    Node *p, *c;
+    p = head;
+    c = head->right;
+    while (c != end)
+    {
+        if (num < c->key)
+        {
+            p = c;
+            c = c->left;
+        }
+        else if (num > c->key)
+        {
+            p = c;
+            c = c->right;
+        }
+        else
+        {
+            break;
+        }
+    }
+    if (c == end)
+        return;
+    Node *temp = c;
+    if (c->left == NULL && c->right == NULL)
+    {
+        c = end;
+    }
+    else if (c->left != NULL && c->right == NULL)
+    {
+        c = c->left;
+    }
+    else if (c->left == NULL && c->right != NULL)
+    {
+        c = c->right;
+    }
+    else
+    {
+        Node *g = g->right;
+        while (c->left->left != end)
+        {
+            g = g->left;
+        }
+        c = g->left;
+        g->left = c->right;
+        c->left = temp->left;
+        c->right = temp->right;
+    }
+    delete temp;
+    if (num > p->key)
+        p->left = c;
+    else
+        p->right = c;
 }
 
 int main()

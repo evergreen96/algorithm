e80f525306d843a4c1f1924d4ca78eb391f1479b
evergreen96 <jsr27382@gmail.com>
2020-08-09 11:00:04 +0900

    re save

--- bj_2751.cpp
+++ bj_2751.cpp
@@ -32,7 +32,7 @@ void qsort(int start, int end)
 
 int main()
 {
-    ios_base ::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     int n;
--- bj_2957.cpp
+++ bj_2957.cpp
@@ -3,39 +3,38 @@
 */
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <map>
-
+#include <vector>
 using namespace std;
 
-int arr[300001]={0,};
-
-
+int arr[300001] = {
+    -1,
+};
 
 int main() {
+
   int max, num, loop = 0, count = 0, root;
   cin >> max;
 
   while (loop++ < max) {
     cin >> num;
-    if (loop == 1) {
+    if (count == 0) {
       root = num;
       arr[num] = 0;
       cout << count << endl;
     } else {
       int i;
       if (num > root) {
-        for (i = num-1; i > root; i--) {
-          if (arr[i] != 0)
+        for (i = root + 1; i < num; i++) {
+          if (arr[i] != -1)
             break;
         }
         arr[num] = arr[i] + 1;
         count += arr[num];
         cout << count << endl;
       } else {
-        for (i = num+1; i <root; i++) {
-          if (arr[i] != 0)
+        for (i = root - 1; i > 0; i--) {
+          if (arr[i] != -1)
             break;
         }
         arr[num] = arr[i] + 1;
